5a3995b355923e7f325f335557d1ebccdcce2660
Figby222 <figby222@outlook.com>
2024-09-22 15:14:44 -0700

    Serialize & deserialize User

---
+++ config/passport.mjs
@@ -0,0 +1,41 @@
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import bcrypt from "bcryptjs";
+import pool from "../db/pool.mjs";
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query(`SELECT * FROM users WHERE username = $1` [username]);
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: "Incorrect username or password" });
+            }
+
+            const isMatch = bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return done(null, false, { message: "Incorrect username or password" });
+            }
+            return done(null, user);
+        } catch (err) {
+            done(err);
+        }
+    })
+)
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser(async (userId, done) => {
+    try {
+        const { rows } = await pool.query(`SELECT * FROM users WHERE users.id = $1`, [userId]);
+        const user = rows[1];
+        
+        done(null, user);
+    } catch(err) {
+        done(err);
+    }
+})
